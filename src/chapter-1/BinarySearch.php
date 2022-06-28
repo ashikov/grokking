ab60feaa0e84f6e6a9b5184047c47fa6d2e95bdf
Roman Ashikov <rmn.shkv@gmail.com>
2022-06-28 12:54:03 +0300

    add binary search

--- src/chapter-1/BinarySearch.php
+++ src/chapter-1/BinarySearch.php
@@ -2,7 +2,26 @@
 
 namespace Grokking\BinarySearch;
 
+// Function returns index
 function binarySearch(array $haystack, mixed $niddle): mixed
 {
-    return true;
+    // Search borders
+    $firstIndex = 0;
+    $lastIndex = count($haystack) - 1;
+
+    while ($firstIndex <= $lastIndex) {
+        $middleIndex = $firstIndex + $lastIndex;
+
+        if ($haystack[$middleIndex] === $niddle) {
+            return $middleIndex;
+        }
+
+        if ($haystack[$middleIndex] > $niddle) {
+            $lastIndex = $middleIndex - 1;
+        } else {
+            $firstIndex = $middleIndex + 1;
+        }
+    }
+
+    return null;
 }
--- tests/chapter-1/BinarySearchTest.php
+++ tests/chapter-1/BinarySearchTest.php
@@ -10,7 +10,8 @@ class BinarySearchTest extends TestCase
 {
     public function testBinarySearch(): void
     {
-        $collection = [1, 2, 3, 4, 5, 6, 7];
-        $this->assertTrue(binarySearch($collection, 5));
+        $collection = [1, 3, 5, 7, 9];
+        $this->assertEquals(1, binarySearch($collection, 3));
+        $this->assertEquals(null, binarySearch($collection, 100));
     }
 }
